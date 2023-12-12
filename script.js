8d618cb8252c48503561275c77c39a50cc101fef
Claudia González <claudia.glezg@gmail.com>
2023-12-12 20:00:43 +0100

    first design added

---
+++ script.js
@@ -0,0 +1,60 @@
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+jokeBtn.addEventListener('click', generateJoke);
+
+function generateJoke() {
+      const config = {
+        headers: {
+          Accept: 'application/json',
+         }
+   }
+   fetch('https://icanhazdadjoke.com', config)
+    .then((res) => res.json())
+    .then((data) => {
+      jokeEl.innerHTML = data.joke;
+     })
+}
+
+
+
+
+
+
+
+
+
+
+
+// USING ASYNC/AWAIT
+/*
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: '',
+    },
+  }
+
+  const res = await ('', config)
+
+  const data = await res.json()
+
+  jokeEl.innerHTML = data.joke
+}
+
+//   HINTS
+// USING .then()
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       Accept: 'application/json',
+//     },
+//   }
+
+//   >>fetch('https://icanhazdadjoke.com', config)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       jokeEl.innerHTML = data.joke
+//     })
+// }
+*/
